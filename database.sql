a18608935eaf9588780abdc39643b33ae886c2da
Papin Semyon <s.papin@sftpro.ru>
2022-10-15 00:17:36 +0300

    add sql scripts for creating tables

--- database.sql
+++ database.sql
@@ -1,5 +1,56 @@
-create table users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    chat_id INTEGER
+CREATE TABLE IF NOT EXISTS public.messages
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    value text COLLATE pg_catalog."default" NOT NULL,
+    user_id integer NOT NULL,
+    chat_id integer NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT messages_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_chat FOREIGN KEY (chat_id)
+        REFERENCES public.chats (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_user FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
+
+
+CREATE TABLE IF NOT EXISTS public.chats
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    telegram_chat_id integer NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT chats_pkey PRIMARY KEY (id),
+    CONSTRAINT chats_telegram_chat_id_key UNIQUE (telegram_chat_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.chats_users
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    chat_id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT chats_users_pkey PRIMARY KEY (id),
+    CONSTRAINT chats_users_chat_id_user_id_key UNIQUE (chat_id, user_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    username character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    first_name character varying(100) COLLATE pg_catalog."default",
+    last_name character varying(100) COLLATE pg_catalog."default",
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_username_key UNIQUE (username)
+)
